71cf4a78087a1642ec84a7e25f6cbbad720540ad
Kiloloan <kiloloan@users.noreply.github.com>
2023-02-11 22:48:45 +0100

    update :D

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-basic.show_icon(IconNames.HAPPY)
-LCD1IN8.LCD_Init()
-LCD1IN8.LCD_Clear()
-LCD1IN8.LCD_Display()
-basic.show_icon(IconNames.YES)
-
-def on_forever():
-    basic.show_icon(IconNames.NO)
+def afficheLog(texte: str):
+    basic.show_icon(IconNames.CHESSBOARD)
+    LCD1IN8.draw_rectangle(0,
+        115,
+        160,
+        128,
+        LCD1IN8.Get_Color(LCD_COLOR.GREEN),
+        DRAW_FILL.DRAW_FULL,
+        DOT_PIXEL.DOT_PIXEL_1)
+    LCD1IN8.dis_string(1, 116, texte, LCD1IN8.Get_Color(LCD_COLOR.BLACK))
+    LCD1IN8.LCD_DisplayWindows(0, 115, 160, 128)
+def fond_ecran():
+    afficheLog("affiche ligne")
+    basic.show_icon(IconNames.STICK_FIGURE)
     for AxeX in range(161):
         LCD1IN8.draw_line(AxeX,
             1,
@@ -15,6 +21,8 @@ def on_forever():
             DOT_PIXEL.DOT_PIXEL_1,
             LINE_STYLE.LINE_SOLID)
     LCD1IN8.LCD_Display()
+    afficheLog("affiche rectangle")
+    basic.show_icon(IconNames.STICK_FIGURE)
     for AxeY in range(65):
         LCD1IN8.draw_rectangle(AxeY,
             AxeY,
@@ -24,6 +32,8 @@ def on_forever():
             DRAW_FILL.DRAW_EMPTY,
             DOT_PIXEL.DOT_PIXEL_1)
     LCD1IN8.LCD_Display()
+    afficheLog("affiche cercle")
+    basic.show_icon(IconNames.STICK_FIGURE)
     for radius in range(33):
         AxeY2 = 0
         LCD1IN8.draw_circle(96,
@@ -34,4 +44,63 @@ def on_forever():
             DOT_PIXEL.DOT_PIXEL_1)
     LCD1IN8.LCD_Display()
     basic.show_icon(IconNames.YES)
+
+def on_button_pressed_a():
+    global posX
+    basic.show_leds("""
+        . # . . .
+                . # # . .
+                . # # # .
+                . # # . .
+                . # . . .
+    """)
+    if posX < 160:
+        posX += 1
+    else:
+        posX = 1
+    AfficheCurseur()
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global poxY
+    basic.show_leds("""
+        . . . . .
+                # # # # #
+                . # # # .
+                . . # . .
+                . . . . .
+    """)
+    if poxY < 128:
+        poxY += 1
+    else:
+        poxY = 1
+    AfficheCurseur()
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def AfficheCurseur():
+    afficheLog("X" + str(posX) + "|Y" + str(poxY))
+    LCD1IN8.draw_point(posX, poxY, color, DOT_PIXEL.DOT_PIXEL_2)
+    LCD1IN8.LCD_DisplayWindows(posX - 2, poxY - 2, posX + 2, poxY + 2)
+color = 0
+poxY = 0
+posX = 0
+basic.show_icon(IconNames.HAPPY)
+posX = 1
+poxY = 1
+color = 0
+LCD1IN8.LCD_Init()
+LCD1IN8.LCD_Clear()
+LCD1IN8.LCD_Display()
+afficheLog("initialisation")
+basic.show_icon(IconNames.YES)
+fond_ecran()
+basic.show_icon(IconNames.NO)
+
+def on_forever():
+    global color
+    basic.show_icon(IconNames.SQUARE)
+    if color <= 65536:
+        color += 1
+    else:
+        color = 0
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 function afficheLog (texte: string) {
+    basic.showIcon(IconNames.Chessboard)
     LCD1IN8.DrawRectangle(
     0,
     115,
@@ -21,15 +22,9 @@ function afficheLog (texte: string) {
     128
     )
 }
-basic.showIcon(IconNames.Happy)
-LCD1IN8.LCD_Init()
-LCD1IN8.LCD_Clear()
-LCD1IN8.LCD_Display()
-afficheLog("initialisation")
-basic.showIcon(IconNames.Yes)
-basic.forever(function () {
-    basic.showIcon(IconNames.No)
+function fond_ecran () {
     afficheLog("affiche ligne")
+    basic.showIcon(IconNames.StickFigure)
     for (let AxeX = 0; AxeX <= 160; AxeX++) {
         LCD1IN8.DrawLine(
         AxeX,
@@ -43,6 +38,7 @@ basic.forever(function () {
     }
     LCD1IN8.LCD_Display()
     afficheLog("affiche rectangle")
+    basic.showIcon(IconNames.StickFigure)
     for (let AxeY = 0; AxeY <= 64; AxeY++) {
         LCD1IN8.DrawRectangle(
         AxeY,
@@ -56,6 +52,7 @@ basic.forever(function () {
     }
     LCD1IN8.LCD_Display()
     afficheLog("affiche cercle")
+    basic.showIcon(IconNames.StickFigure)
     for (let radius = 0; radius <= 32; radius++) {
         let AxeY = 0
         LCD1IN8.DrawCircle(
@@ -69,4 +66,82 @@ basic.forever(function () {
     }
     LCD1IN8.LCD_Display()
     basic.showIcon(IconNames.Yes)
+}
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . # . . .
+        . # # . .
+        . # # # .
+        . # # . .
+        . # . . .
+        `)
+    if (posX < 160) {
+        posX += 5
+    } else {
+        posX = 1
+    }
+    AfficheCurseur()
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.SmallSquare)
+    afficheLog("X" + posX + "|Y" + poxY)
+    LCD1IN8.DrawCircle(
+    posX,
+    poxY,
+    32,
+    color,
+    DRAW_FILL.DRAW_FULL,
+    DOT_PIXEL.DOT_PIXEL_1
+    )
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        # # # # #
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    if (poxY < 128) {
+        poxY += 5
+    } else {
+        poxY = 1
+    }
+    AfficheCurseur()
+})
+function AfficheCurseur () {
+    LCD1IN8.DrawPoint(
+    posX,
+    poxY,
+    color,
+    DOT_PIXEL.DOT_PIXEL_2
+    )
+    LCD1IN8.LCD_DisplayWindows(
+    posX - 2,
+    poxY - 2,
+    posX + 2,
+    poxY + 2
+    )
+}
+let color = 0
+let poxY = 0
+let posX = 0
+basic.showIcon(IconNames.Happy)
+posX = 1
+poxY = 1
+color = 0
+LCD1IN8.LCD_Init()
+LCD1IN8.LCD_Clear()
+LCD1IN8.LCD_Display()
+afficheLog("initialisation")
+basic.showIcon(IconNames.Yes)
+fond_ecran()
+basic.showIcon(IconNames.No)
+basic.forever(function () {
+    basic.showIcon(IconNames.Square)
+    if (color <= 65536) {
+        color += 1
+    } else {
+        color = 0
+    }
 })
